ac3c4b6650393b4bf4c3ab18721cdb57722ac9ac
Alex Ur <ru.booking@yandex.ru>
2024-09-07 13:00:07 +0300

    Fix response bags 1.3

---
+++ src/components/About.vue
@@ -0,0 +1,46 @@
+<script setup>
+import avatar from "/img/avatar.jpg";
+</script>
+
+<template>
+  <div
+    class="flex justify-between items-center mb-64 w-full max-[880px]:flex-col max-[770px]:mb-[150px]"
+  >
+    <div>
+      <h2
+        class="main-title text-title-color font-bold text-6xl text-start w-auto h-72 font-bold mt-5 max-[865px]:text-6xl max-[565px]:text-5xl text-center"
+      >
+        Hi 👋, <br />
+        My name is <br />
+        <span>Pavan MG</span> <br />
+        I build things for web
+      </h2>
+    </div>
+
+    <div
+      class="main__avatar-circle w-[75%]] max-[880px]:mt-5 max-[700px]:w-[65%]"
+    >
+      <img class="rounded-full" :src="avatar" alt="Profile Pic" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.main-title {
+  font-family: "Poppins", sans-serif;
+  font-style: normal;
+
+  span {
+    background: linear-gradient(90deg, #13b0f5 -2.06%, #e70faa 100%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+}
+
+.main__avatar-circle {
+  background: -webkit-linear-gradient(left top, #13b0f5 -2.06%, #e70faa 100%);
+  border-radius: 1000px;
+  padding: 10px;
+  position: relative;
+}
+</style>
